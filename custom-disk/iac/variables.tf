ccc8259d1c0c2133df13c1e8c4de4bbad206f081
fvilarin <fvilarin@akamai.com>
2024-05-06 16:48:36 -0300

    Initial commit.

--- custom-disk/iac/variables.tf
+++ custom-disk/iac/variables.tf
@@ -9,15 +9,3 @@ variable "settingsFilename" {
   type = string
   default = "settings.json"
 }
-
-# SSH private key filename.
-variable "sshPrivateKeyFilename" {
-  type = string
-  default = "~/.ssh/id_rsa"
-}
-
-# SSH public key filename.
-variable "sshPublicKeyFilename" {
-  type = string
-  default = "~/.ssh/id_rsa.pub"
-}
